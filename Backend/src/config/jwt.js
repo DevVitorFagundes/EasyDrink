e96aae316c382287b0cac8585ea7e3b80542d83f
Vitor Fagundes <vitorfagundes575@gmail.com>
2025-11-19 13:11:07 -0300

    add auth and backend

---
+++ Backend/src/config/jwt.js
@@ -0,0 +1,4 @@
+export const jwtConfig = {
+  secret: "minha_chave_secreta_super_simples",
+  expiresIn: "2h"
+};
---
+++ Backend/src/middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import { jwtConfig } from "../config/jwt.js";
+
+export const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader)
+    return res.status(401).json({ error: "Token não enviado" });
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, jwtConfig.secret);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Token inválido" });
+  }
+};
