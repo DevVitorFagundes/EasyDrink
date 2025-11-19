91f6f8e408d94bd5b9ba099d163fbcfa00db840a
Guilherme Fonseca <guilhermelimafonseca@gmail.com>
2025-11-19 16:44:38 -0300

    Revert "add auth and backend"
    This reverts commit e96aae316c382287b0cac8585ea7e3b80542d83f.

--- Backend/src/config/jwt.js
+++
@@ -1,4 +0,0 @@
-export const jwtConfig = {
-  secret: "minha_chave_secreta_super_simples",
-  expiresIn: "2h"
-};
--- Backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { login } from "../controllers/auth.controller.js";
-
-const router = Router();
-
-// POST /auth/login
-router.post("/login", login);
-
-export default router;
